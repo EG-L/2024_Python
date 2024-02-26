17fc698a5509c72ddccdce0eaf6ec6e8f5df56b8
EG-L <pshowx12@naver.com>
2024-02-26 15:50:49 +0900

    20240226

--- basic2.py
+++ basic2.py
@@ -11,3 +11,15 @@ print(f"b={b},c는{c},d는{d}")
 # SQL => f"SELECT * FROM emp WHERE empno={empno}"
 
 print(id(b),id(c),id(d))
+# 같은 값을 가지고 있는 경우에는 변수의 주소가 동일
+a1 = 3
+b1 = 3
+print(a1 is b1)
+print(a1 == b1)
+
+c1 = "Hello "
+print("c1:%s"%c1,end="Python\n")
+# end => \t , \n
+# end => seq(구분자)
+print("a:%d" %a,"b:%f" %b,"c:%s" %c,sep="-")
+
